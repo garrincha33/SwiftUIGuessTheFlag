1804e3281116d2d406765da185c9a869d58e6276
Richard Price <richyprice77@gmail.com>
2022-01-26 14:20:02 +0000

    inital commit

---
+++ SwiftUIGuessTheFlag/SwiftUIGuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGuessTheFlagApp.swift
+//  SwiftUIGuessTheFlag
+//
+//  Created by Richard Price on 26/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
