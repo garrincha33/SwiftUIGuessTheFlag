410c21972a22c33199e2fb5506f282e0b417822a
Richard Price <richyprice77@gmail.com>
2022-01-28 06:39:09 +0000

    added some state for score and showing alert continuing game

--- SwiftUIGuessTheFlag/ContentView.swift
+++ SwiftUIGuessTheFlag/ContentView.swift
@@ -8,8 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
-    var correctAnswer = Int.random(in: 0...2)
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var score = 0
+    
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
 
     var body: some View {
         
@@ -25,7 +29,7 @@ struct ContentView: View {
                 
                 ForEach(0..<3) { number in
                     Button {
-                       // flag was tapped
+                       FlagTapped(number)
                     } label: {
                         Image(countries[number])
                             .resizable()
@@ -36,6 +40,27 @@ struct ContentView: View {
                 }
             }
         }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: AskQuestion)
+        } message: {
+            Text("your score is \(score)")
+        }
+    }
+    
+    func FlagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            score += 1
+        } else {
+            scoreTitle = "Wrong"
+        }
+        
+        showingScore = true
+    }
+    
+    func AskQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0..<2)
     }
 }
 
